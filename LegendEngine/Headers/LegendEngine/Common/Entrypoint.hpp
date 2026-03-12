2648591f26e414540291dc8551960770ab3c8b6f
BendedWills <bendedwills@gmail.com>
2026-03-12 11:30:09 -0500

    w

--- LegendEngine/Headers/LegendEngine/Common/Entrypoint.hpp
+++ LegendEngine/Headers/LegendEngine/Common/Entrypoint.hpp
@@ -4,7 +4,7 @@
 #include <Tether/Platform/Win32Window.hpp>
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
-#define LEGENDENGINE_MAIN int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+#define LEGENDENGINE_MAIN int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 #else
 #define LEGENDENGINE_MAIN int main()
 #endif
