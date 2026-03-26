428648f51a5e238bb2b0052453a864e6447bde31
BendedWills <bendedwills@gmail.com>
2026-03-26 10:33:12 -0500

    Delete old mesh component

--- LegendEngine/Headers/LE/Resources/MeshData.hpp
+++ LegendEngine/Headers/LE/Resources/MeshData.hpp
@@ -8,17 +8,30 @@ namespace le
     class MeshData : public Resource
     {
     public:
+        enum class UpdateFrequency
+        {
+            UPDATES_ONCE,
+            UPDATES_OCCASIONALLY,
+            UPDATES_OFTEN,
+        };
+
         struct Vertex3
         {
             float position[3];
             float texcoord[3];
         };
 
-        MeshData();
+        MeshData(std::span<Vertex3> vertices, std::span<uint32_t> indices,
+            UpdateFrequency updateFrequency);
+        MeshData(size_t initialVertexCount, size_t initialIndexCount,
+            UpdateFrequency frequency);
 
         ID<MeshData> id = ID<MeshData>(m_uid);
     private:
         Scope<Buffer> m_vertexBuffer;
         Scope<Buffer> m_indexBuffer;
+
+        size_t m_vertexCount;
+        size_t m_indexCount;
     };
 }
